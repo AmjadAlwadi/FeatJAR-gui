2bf3f2811df110854a997d2a2462c8d324f781b9
Amjad Alwadi <aamjadalwadi@gmail.com>
2025-10-14 13:37:19 +0200

    removing all unnecessary files

--- glsp-dependencies/glsp-client/tasklist-glsp/lib/my-custom-shape-view.d.ts
+++
@@ -1,6 +0,0 @@
-import { ShapeView } from '@eclipse-glsp/client';
-import type { VNode } from 'snabbdom';
-export declare class MyCustomShapeView extends ShapeView {
-    render(): VNode;
-}
-//# sourceMappingURL=my-custom-shape-view.d.ts.map
--- glsp-dependencies/glsp-client/tasklist-glsp/lib/tasklist-diagram-module.d.ts
+++
@@ -1,5 +0,0 @@
-import { ContainerConfiguration } from '@eclipse-glsp/client';
-import { Container } from 'inversify';
-import '../css/diagram.css';
-export declare function initializeTasklistDiagramContainer(container: Container, ...containerConfiguration: ContainerConfiguration): Container;
-//# sourceMappingURL=tasklist-diagram-module.d.ts.map
